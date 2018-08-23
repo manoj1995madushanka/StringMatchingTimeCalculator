f86e5b4000d94269797fe11e26237cb5d6d5a903
manoj1995madushanka <madushankamanoj414@gmail.com>
2018-08-23 17:53:01 +0530

    GUI

---
+++ src/stringmatching/StringMatching.java
@@ -0,0 +1,13 @@
+
+package stringmatching;
+
+
+public class StringMatching {
+
+    
+    public static void main(String[] args) {
+        MainFrame mf=new MainFrame();
+        mf.setVisible(true);
+    }
+    
+}
